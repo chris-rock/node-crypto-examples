d2b79448b2f2820adb05a735976eff1c688a214d
Utsav Shah <ukshah2@illinois.edu>
2016-10-21 17:16:15 -0500

    Fix typo

--- sha-text.js
+++ sha-text.js
@@ -3,7 +3,7 @@ var crypto = require('crypto'),
   text = 'hello bob',
   key = 'mysecret key'
 
-// create hahs
+// create hash
 var hash = crypto.createHmac('sha512', key)
 hash.update(text)
 var value = hash.digest('hex')
